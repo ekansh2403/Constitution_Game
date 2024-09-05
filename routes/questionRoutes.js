bb52ea547320760043f584ae0726a4cf2bc1fa64
Your Name <abhirajtiwari189@gmail.com>
2024-09-05 23:36:12 +0530

    deleted all files

--- routes/questionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getQuestions } = require('../controllers/questionController');
-const router = express.Router();
-
-// Get a set of random questions
-router.get('/', getQuestions);
-
-module.exports = router;
--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { submitAnswer } = require('../controllers/userController');
-const router = express.Router();
-
-// Submit user's answer and update score
-router.post('/submit-answer', submitAnswer);
-
-module.exports = router;
